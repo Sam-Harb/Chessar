7d81c4f800f3f8ca94c2b04e2d1eacc70ac28fda
Samsara Harb <samsaraharbb@gmail.com>
2024-11-03 11:34:31 -0500

    Se agregó la lógica del tablero a Boardjs

--- src/app/Components/Board.js
+++ src/app/Components/Board.js
@@ -1,3 +1,14 @@
 import React from 'react';
 import Square from './Square';
 import Styles from './Board.module.css';
+
+const Board=()=>{
+  const board = Array.from ({length: 8}, (_, row) => (
+  <div key={row} className={styles.row}>
+    {Array.from ({length: 8}, (_, col) =>(
+      <Square key={`${row}-${col}`} row={row} col ={col}/>
+    ))}
+  </div>
+);
+return <div>{board}</div>;
+}
