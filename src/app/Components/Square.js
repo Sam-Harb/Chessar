e1eb5d1804d2cb2fb670e7030586f7f1fcacb314
Samsara Harb <samsaraharbb@gmail.com>
2024-11-03 19:35:57 -0500

    se agregó la lógica de square

--- src/app/Components/Square.js
+++ src/app/Components/Square.js
@@ -1,2 +1,12 @@
 import React from 'react';
 import Styles from './Board.module.css';
+
+
+const Square=({ row, col }) =>{
+  const isBlack = (col + row) % 2 === 1;
+  const squareClass = isBlack ? styles.black : styles.white;
+
+  return <div clasName={`${styles.square} ${squareClass}`}></div>
+}
+
+export default Square;
