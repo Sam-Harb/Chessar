6ea8a1ed38fe9950af943e2f2d902ced08379df3
Samsara Harb <samsaraharbb@gmail.com>
2024-11-05 20:49:47 -0500

    se modificó pagejs con el mensaje de chessart y los créditos de subtitulo

--- src/app/page.js
+++ src/app/page.js
@@ -4,7 +4,8 @@ import Board from './Components/Board.js';
 export default function Page() {
   return (
     <div>
-      <h1>¡Bienvenido a mi Proyecto de Ajedrez!</h1>
+      <h1>Chessart</h1>
+        <h2>El primer proyecto en Next de Samsara Harb, noviembre 2024</h2>
       <Board />
     </div>
   );
